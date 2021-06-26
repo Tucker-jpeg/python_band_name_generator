1bf195a1f8b60cc2d5660b1e34b1685c342c1e86
Tucker-jpeg <74992550+Tucker-jpeg@users.noreply.github.com>
2021-06-26 10:47:06 -0400

    Add files via upload

---
+++ band_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7.6
+# Python implementation here
+
+#create a greeting
+print("Welcome to the Band Name Generator")
+
+#Ask the user for the city that they grew up in.
+city = input("What's name of the city you grew up in?\n")
+
+#Ask the user for the name of their favorite food.
+food_name = input("What's your favorite food or drink?\n")
+
+#Ask the user for an obscure occupation
+kicker = input("Name a random object or animal\n")
+
+#Combine the name of their city, food, and object and show them their band name.
+print("Your band name could be " + city + " " + food_name + " " + kicker)
